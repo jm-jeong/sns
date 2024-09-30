fbecddad5df388c786a9fd472d0d9de30f162edb
jaemin <jjm7655@gmail.com>
2024-10-01 02:47:33 +0900

    feat: 포스트 삭제시 코멘트, 라이크 같이 삭제되도록 수정

--- src/main/java/com/fast/campus/simplesns/repository/CommentEntityRepository.java
+++ src/main/java/com/fast/campus/simplesns/repository/CommentEntityRepository.java
@@ -3,6 +3,10 @@ package com.fast.campus.simplesns.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fast.campus.simplesns.model.entity.CommentEntity;
 import com.fast.campus.simplesns.model.entity.PostEntity;
@@ -10,4 +14,8 @@ import com.fast.campus.simplesns.model.entity.PostEntity;
 public interface CommentEntityRepository extends JpaRepository<CommentEntity, Integer> {
 	Page<CommentEntity> findAllByPost(PostEntity post, Pageable pageable);
 
+	@Transactional
+	@Modifying
+	@Query("UPDATE CommentEntity entity SET removed_at = NOW() where entity.post = :post")
+	void deleteAllByPost(@Param("post") PostEntity post);
 }
--- src/main/java/com/fast/campus/simplesns/repository/LikeEntityRepository.java
+++ src/main/java/com/fast/campus/simplesns/repository/LikeEntityRepository.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fast.campus.simplesns.model.entity.LikeEntity;
 import com.fast.campus.simplesns.model.entity.PostEntity;
@@ -19,4 +21,9 @@ public interface LikeEntityRepository extends JpaRepository<LikeEntity, Integer>
 	Integer countByPost(@Param("post") PostEntity post);
 
 	List<LikeEntity> findAllByPost(PostEntity post);
+
+	@Transactional
+	@Modifying
+	@Query("UPDATE LikeEntity entity SET removed_at = NOW() where entity.post = :post")
+	void deleteAllByPost(@Param("post") PostEntity post);
 }
