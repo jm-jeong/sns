dda57ee0e862f310013038af3507a9d19eb6e68e
jaemin <jjm7655@gmail.com>
2024-09-30 16:04:57 +0900

    feat: post 기능 controller, service test cod add

---
+++ src/test/java/com/fast/campus/simplesns/controller/PostControllerTest.java
@@ -0,0 +1,144 @@
+package com.fast.campus.simplesns.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fast.campus.simplesns.controller.request.PostWriteRequest;
+import com.fast.campus.simplesns.exception.ErrorCode;
+import com.fast.campus.simplesns.exception.SimpleSnsApplicationException;
+import com.fast.campus.simplesns.service.PostService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private PostService postService;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	@WithMockUser
+	void 포스트작성() throws Exception {
+		mockMvc.perform(post("/api/v1/posts")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(new PostWriteRequest("title", "body"))))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
+
+	@Test
+	@WithAnonymousUser
+	void 포스트작성시_로그인한상태가_아니라면_에러발생() throws Exception {
+		mockMvc.perform(post("/api/v1/posts")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(new PostWriteRequest("title", "body"))))
+			.andDo(print())
+			.andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()));
+	}
+
+	@Test
+	@WithAnonymousUser
+	void 포스트수정시_로그인한상태가_아니라면_에러발생() throws Exception {
+		mockMvc.perform(put("/api/v1/posts/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(new PostWriteRequest("title", "body"))))
+			.andDo(print())
+			.andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()));
+	}
+
+	@Test
+	@WithMockUser
+	void 포스트수정시_본인이_작성한_글이_아니라면_에러발생() throws Exception {
+		doThrow(new SimpleSnsApplicationException(ErrorCode.INVALID_PERMISSION)).when(postService).modify(any(), eq(1), eq("title"), eq("body"));
+		mockMvc.perform(put("/api/v1/posts/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(new PostWriteRequest("title", "body"))))
+			.andDo(print())
+			.andExpect(status().is(ErrorCode.INVALID_PERMISSION.getStatus().value()));
+	}
+
+	@Test
+	@WithMockUser
+	void 포스트수정시_수정하려는글이_없다면_에러발생() throws Exception {
+		doThrow(new SimpleSnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService).modify(any(), eq(1), eq("title"), eq("body"));
+		mockMvc.perform(put("/api/v1/posts/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(new PostWriteRequest("title", "body"))))
+			.andDo(print())
+			.andExpect(status().is(ErrorCode.POST_NOT_FOUND.getStatus().value()));
+	}
+
+	@Test
+	@WithMockUser
+	void 포스트수정시_데이터베이스_에러_발생시_에러발생() throws Exception {
+		doThrow(new SimpleSnsApplicationException(ErrorCode.DATABASE_ERROR)).when(postService).modify(any(), eq(1), eq("title"), eq("body"));
+		mockMvc.perform(put("/api/v1/posts/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(new PostWriteRequest("title", "body"))))
+			.andDo(print())
+			.andExpect(status().is(ErrorCode.DATABASE_ERROR.getStatus().value()));
+	}
+
+	@Test
+	@WithAnonymousUser
+	void 포스트삭제시_로그인한상태가_아니라면_에러발생() throws Exception {
+		mockMvc.perform(delete("/api/v1/posts/1")
+				.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()));
+	}
+
+	@Test
+	@WithMockUser
+	void 포스트삭제시_본인이_작성한_글이_아니라면_에러발생() throws Exception {
+		doThrow(new SimpleSnsApplicationException(ErrorCode.INVALID_PERMISSION)).when(postService).delete(any(), eq(1));
+		mockMvc.perform(delete("/api/v1/posts/1")
+				.header(HttpHeaders.AUTHORIZATION, "Bearer token")
+				.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().is(ErrorCode.INVALID_PERMISSION.getStatus().value()));
+	}
+
+	@Test
+	@WithMockUser
+	void 포스트삭제시_수정하려는글이_없다면_에러발생() throws Exception {
+		doThrow(new SimpleSnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService).delete(any(), eq(1));
+
+		mockMvc.perform(delete("/api/v1/posts/1")
+				.header(HttpHeaders.AUTHORIZATION, "Bearer token")
+				.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().is(ErrorCode.POST_NOT_FOUND.getStatus().value()));
+	}
+
+	@Test
+	@WithMockUser
+	void 포스트삭제시_데이터베이스_에러_발생시_에러발생() throws Exception {
+		doThrow(new SimpleSnsApplicationException(ErrorCode.DATABASE_ERROR)).when(postService).delete(any(), eq(1));
+		mockMvc.perform(delete("/api/v1/posts/1")
+				.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().is(ErrorCode.DATABASE_ERROR.getStatus().value()));
+	}
+
+}
---
+++ src/test/java/com/fast/campus/simplesns/service/PostServiceTest.java
@@ -0,0 +1,131 @@
+package com.fast.campus.simplesns.service;
+
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Pageable;
+
+import com.fast.campus.simplesns.exception.ErrorCode;
+import com.fast.campus.simplesns.exception.SimpleSnsApplicationException;
+import com.fast.campus.simplesns.fixture.TestInfoFixture;
+import com.fast.campus.simplesns.fixture.UserEntityFixture;
+import com.fast.campus.simplesns.model.entity.PostEntity;
+import com.fast.campus.simplesns.model.entity.UserEntity;
+import com.fast.campus.simplesns.repository.PostEntityRepository;
+import com.fast.campus.simplesns.repository.UserEntityRepository;
+
+@SpringBootTest
+public class PostServiceTest {
+
+	@Autowired
+	PostService postService;
+
+	@MockBean
+	UserEntityRepository userEntityRepository;
+
+	@MockBean
+	PostEntityRepository postEntityRepository;
+
+	@Test
+	void 포스트_생성시_정상동작한다() {
+		TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+		when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(
+			Optional.of(UserEntityFixture.get(fixture.getUserName(), fixture.getPassword())));
+		when(postEntityRepository.save(any())).thenReturn(mock(PostEntity.class));
+		Assertions.assertDoesNotThrow(
+			() -> postService.create(fixture.getUserName(), fixture.getTitle(), fixture.getBody()));
+	}
+
+	@Test
+	void 포스트생성시_유저가_존재하지_않으면_에러를_내뱉는다() throws Exception {
+		TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+		when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.empty());
+		SimpleSnsApplicationException exception = Assertions.assertThrows(
+			SimpleSnsApplicationException.class,
+			() -> postService.create(fixture.getUserName(), fixture.getTitle(), fixture.getBody()));
+		Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+	}
+
+	@Test
+	void 내_포스트리스트를_가져올_유저가_존재하지_않으면_에러를_내뱉는다() {
+		TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+		when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.empty());
+		SimpleSnsApplicationException exception = Assertions.assertThrows(
+			SimpleSnsApplicationException.class,
+			() -> postService.my(fixture.getUserName(), mock(Pageable.class)));
+		Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+	}
+
+	@Test
+	void 포스트_수정시_포스트가_존재하지_않으면_에러를_내뱉는다() {
+		TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+		when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.of(UserEntityFixture.get(fixture.getUserName(), fixture.getPassword())));
+		when(postEntityRepository.findById(fixture.getPostId())).thenReturn(Optional.empty());
+		SimpleSnsApplicationException exception = Assertions.assertThrows(
+			SimpleSnsApplicationException.class,
+			() -> postService.modify(fixture.getUserName(), fixture.getPostId(), fixture.getTitle(), fixture.getBody()));
+		Assertions.assertEquals(ErrorCode.POST_NOT_FOUND, exception.getErrorCode());
+	}
+
+	@Test
+	void 포스트_수정시_유저가_존재하지_않으면_에러를_내뱉는다() {
+		TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+		when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.empty());
+		SimpleSnsApplicationException exception = Assertions.assertThrows(
+			SimpleSnsApplicationException.class,
+			() -> postService.modify(fixture.getUserName(), fixture.getPostId(), fixture.getTitle(), fixture.getBody()));
+		Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+	}
+
+	@Test
+	void 포스트_수정시_포스트_작성자와_유저가_일치하지_않으면_에러를_내뱉는다() {
+		PostEntity mockPostEntity = mock(PostEntity.class);
+		UserEntity mockUserEntity = mock(UserEntity.class);
+		TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+		when(postEntityRepository.findById(fixture.getPostId())).thenReturn(Optional.of(mockPostEntity));
+		when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.of(mockUserEntity));
+		when(mockPostEntity.getUser()).thenReturn(mock(UserEntity.class));
+		SimpleSnsApplicationException exception = Assertions.assertThrows(
+			SimpleSnsApplicationException.class,
+			() -> postService.modify(fixture.getUserName(), fixture.getPostId(), fixture.getTitle(), fixture.getBody()));
+		Assertions.assertEquals(ErrorCode.INVALID_PERMISSION, exception.getErrorCode());
+	}
+
+	@Test
+	void 포스트_삭제시_포스트가_존재하지_않으면_에러를_내뱉는다() {
+		TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+		when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.of(UserEntityFixture.get(fixture.getUserName(), fixture.getPassword())));
+		when(postEntityRepository.findById(fixture.getPostId())).thenReturn(Optional.empty());
+		SimpleSnsApplicationException exception = Assertions.assertThrows(SimpleSnsApplicationException.class, () -> postService.delete(fixture.getUserName(), fixture.getPostId()));
+		Assertions.assertEquals(ErrorCode.POST_NOT_FOUND, exception.getErrorCode());
+	}
+
+	@Test
+	void 포스트_삭제시_유저가_존재하지_않으면_에러를_내뱉는다() {
+		TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+		when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.empty());
+		SimpleSnsApplicationException exception = Assertions.assertThrows(
+			SimpleSnsApplicationException.class,
+			() -> postService.delete(fixture.getUserName(), fixture.getPostId()));
+		Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+	}
+
+	@Test
+	void 포스트_삭제시_포스트_작성자와_유저가_일치하지_않으면_에러를_내뱉는다() {
+		PostEntity mockPostEntity = mock(PostEntity.class);
+		UserEntity mockUserEntity = mock(UserEntity.class);
+
+		TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+		when(postEntityRepository.findById(fixture.getPostId())).thenReturn(Optional.of(mockPostEntity));
+		when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.of(mockUserEntity));
+		when(mockPostEntity.getUser()).thenReturn(mock(UserEntity.class));
+		SimpleSnsApplicationException exception = Assertions.assertThrows(SimpleSnsApplicationException.class, () -> postService.delete(fixture.getUserName(), fixture.getPostId()));
+		Assertions.assertEquals(ErrorCode.INVALID_PERMISSION, exception.getErrorCode());
+	}
+}
